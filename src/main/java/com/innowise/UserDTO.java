6ec2aaf52baba9cc10ff1b51df0ef51ffa97e0f1
STEPASHKA <i.eshcho.live@gmail.com>
2026-05-17 20:20:03 +0300

    task 2. dao layer created BaseEntity, User and Card entities. created User DTO, Payment Card DTO, User repository and payment card repository. modified application.yml (added validation). added AuditConfig

---
+++ src/main/java/com/innowise/UserDTO.java
@@ -0,0 +1,28 @@
+package com.innowise;
+
+import lombok.Data;
+import jakarta.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+public class UserDTO {
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String name;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String surname;
+
+    @NotNull
+    @Past
+    private LocalDate birthDate;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    private Boolean active;
+}
