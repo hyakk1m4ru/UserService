2224293d1a9e4113c14bd9ed826a6a6d777c8ca2
STEPASHKA <i.eshcho.live@gmail.com>
2026-05-30 15:08:39 +0300

    task 5. caching added redis caching

--- src/main/java/com/innowise/service/PaymentCardService.java
+++ src/main/java/com/innowise/service/PaymentCardService.java
@@ -1,10 +1,8 @@
 package com.innowise.service;
 
 
-
 import com.innowise.dto.PageResponse;
 import com.innowise.dto.PaymentCardDTO;
-import com.innowise.dto.UserDTO;
 import com.innowise.exception.BusinessException;
 import com.innowise.exception.ResourceNotFoundException;
 import com.innowise.mapper.PaymentCardMapper;
@@ -13,10 +11,14 @@ import com.innowise.model.User;
 import com.innowise.repository.PaymentCardRepository;
 import com.innowise.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +27,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly=true)
+@Transactional(readOnly = true)
 public class PaymentCardService {
 
     private final PaymentCardRepository paymentCardRepository;
@@ -33,13 +35,14 @@ public class PaymentCardService {
     private final PaymentCardMapper paymentCardMapper;
 
     @Transactional
-    public PaymentCardDTO createCard(PaymentCardDTO cardDTO){
+    @CacheEvict(value = "user", key = "#cardDTO.userId")
+    public PaymentCardDTO createCard(PaymentCardDTO cardDTO) {
         User user = userRepository.findById(cardDTO.getUserId()).orElseThrow(() -> new ResourceNotFoundException("User", cardDTO.getUserId()));
         long activeCardsCount = paymentCardRepository.countByUserAndActiveTrue(user);
-        if(activeCardsCount >= 5){
+        if (activeCardsCount >= 5) {
             throw new BusinessException("User cant have >= 5 active cards");
         }
-        if(paymentCardRepository.existsByNumber(cardDTO.getNumber())){
+        if (paymentCardRepository.existsByNumber(cardDTO.getNumber())) {
             throw new BusinessException("Card with number " + cardDTO.getNumber() + " already exists");
         }
 
@@ -50,29 +53,33 @@ public class PaymentCardService {
         PaymentCard savedCard = paymentCardRepository.save(card);
         return paymentCardMapper.toDto(savedCard);
     }
-
-    public PaymentCardDTO getCardById(Long id){
-        PaymentCard card = paymentCardRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Payment card", id));
+    @Cacheable(value = "card", key = "#id")
+    public PaymentCardDTO getCardById(Long id) {
+        PaymentCard card = paymentCardRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Payment card", id));
         return paymentCardMapper.toDto(card);
     }
 
-    public PageResponse<PaymentCardDTO> getAllCards(int page, int size){
+    public PageResponse<PaymentCardDTO> getAllCards(int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
         Page<PaymentCard> cardPage = paymentCardRepository.findAll(pageable);
         List<PaymentCardDTO> cardDTOS = cardPage.getContent().stream().map(paymentCardMapper::toDto).toList();
-        return new PageResponse<>(cardDTOS, cardPage.getNumber(),cardPage.getSize(),cardPage.getTotalElements());
+        return new PageResponse<>(cardDTOS, cardPage.getNumber(), cardPage.getSize(), cardPage.getTotalElements());
     }
-    public List<PaymentCardDTO> getCardsByUserId(Long userId){
+
+    @Cacheable(value = "card", key = "#userId")
+    public List<PaymentCardDTO> getCardsByUserId(Long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User", userId));
         return paymentCardRepository.findByUser(user).stream().map(paymentCardMapper::toDto).toList();
     }
 
     @Transactional
-    public PaymentCardDTO updateCard(Long id, PaymentCardDTO cardDTO){
-        PaymentCard card = paymentCardRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Payment card ", id));
+    @CachePut(value = "card", key = "#id")
+    @CacheEvict(value = "userCards", key = "#result.userId")
+    public PaymentCardDTO updateCard(Long id, PaymentCardDTO cardDTO) {
+        PaymentCard card = paymentCardRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Payment card ", id));
 
-        if(!card.getNumber().equals(cardDTO.getNumber()) && paymentCardRepository.existsByNumber(cardDTO.getNumber()) ){
-            throw new BusinessException("Card number "+ cardDTO.getNumber()+" already exists");
+        if (!card.getNumber().equals(cardDTO.getNumber()) && paymentCardRepository.existsByNumber(cardDTO.getNumber())) {
+            throw new BusinessException("Card number " + cardDTO.getNumber() + " already exists");
         }
 
         paymentCardMapper.updateEntityFromDto(cardDTO, card);
@@ -81,16 +88,23 @@ public class PaymentCardService {
     }
 
     @Transactional
-    public void activateCard(Long id){
-        updateCardActiveStatus(id, true);
+    @CachePut(value = "card", key = "#id")
+    @CacheEvict(value = "userCards", key = "#result.userId")
+    public PaymentCardDTO activateCard(Long id) {
+        return updateCardActiveStatus(id, true);
     }
+
     @Transactional
-    public void deactivateCard(Long id){
-        updateCardActiveStatus(id, false);
+    @CachePut(value = "card", key = "#result.userId")
+    @CacheEvict(value = "userCards", key = "#result.userId")
+    public PaymentCardDTO deactivateCard(Long id) {
+        return updateCardActiveStatus(id, false);
     }
-    private void updateCardActiveStatus(Long id, Boolean active){
-        PaymentCard card = paymentCardRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Payment card ", id));
+
+    private PaymentCardDTO updateCardActiveStatus(Long id, Boolean active) {
+        PaymentCard card = paymentCardRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Payment card ", id));
         card.setActive(active);
-        paymentCardRepository.save(card);
+        PaymentCard savedCard = paymentCardRepository.save(card);
+        return paymentCardMapper.toDto(savedCard);
     }
 }
